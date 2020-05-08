7e2b4e22b376e652ee8e0dcf89b93306a131e990
Joshua Saxby <joshua.a.saxby@gmail.com>
2020-05-08 10:45:32 +0100

    Create project template skeleton based on that of DENGR

---
+++ tests/main.cpp
@@ -0,0 +1,7 @@
+/*
+ * This is the Unit-testing entry point as recommended by Catch, the framework
+ * being used for unit-testing:
+ * https://github.com/catchorg/Catch2/blob/master/docs/slow-compiles.md#practical-example
+ */
+#define CATCH_CONFIG_MAIN
+#include "../vendor/catch.hpp"
