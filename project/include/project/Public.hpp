13f41235989997368a0d7c13ef875beff0be5f06
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-03-09 23:46:17 +0000

    Major structure changes for more neat project: - multi-directory source code organisation, separate for headers etc... - use CPM to grab dependencies such as Catch, for easier testing on CI - demonstration of where to put public and private header files - Usage of psuedo-targets in CMake to propagate project-wide options, rather than setting them globally - CMake project is exported properly such that it can be found automatically by other CMake projects once installed (install(EXPORT))

---
+++ project/include/project/Public.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file
+ * This is a sample public compilation unit.
+ *
+ * @author Your Name <your email address>
+ * @date Creation/Edit Date
+ *
+ * @copyright Copyright information goes here
+ *
+ * @copyright
+ * Copyright information can span multiple paragraphs if needed, such as if you
+ * use a well-known software license for which license header text (to be
+ * placed in locations like these) are provided by the license custodians.
+ *
+ */
+
+#ifndef COM_SAXBOPHONE_PROJECT_PUBLIC_HPP
+#define COM_SAXBOPHONE_PROJECT_PUBLIC_HPP
+
+namespace com::saxbophone::project {
+    bool library_works();
+}
+
+#endif // include guard
---
+++ tests/example.cpp
@@ -0,0 +1,9 @@
+#include <catch2/catch.hpp>
+
+#include <project/Public.hpp>
+
+using namespace com::saxbophone::project;
+// test case to check network construction and train movement
+TEST_CASE("Library works") {
+    REQUIRE(library_works());
+}
